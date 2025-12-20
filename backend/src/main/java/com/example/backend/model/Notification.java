f7bab0e6006569d386cd8e2665800a494166a158
simon <nguyensymanh35@gmail.com>
2025-12-20 09:40:35 +0700

    fix table

--- backend/src/main/java/com/example/backend/model/Notification.java
+++ backend/src/main/java/com/example/backend/model/Notification.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name="notification")
+@Table(name="notifications")
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
